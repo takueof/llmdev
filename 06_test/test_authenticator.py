66aa057fcfc0e9f7193eb12228bc755cc5a215ca
Taku Watabe <taku.eof@gmail.com>
2026-02-18 09:50:08 +0900

    テストコードの課題完了

---
+++ 06_test/test_authenticator.py
@@ -0,0 +1,22 @@
+import pytest
+from authenticator import Authenticator
+
+@pytest.fixture
+def authenticator():
+  auth = Authenticator()
+  auth.register('name', 'pass')
+  yield auth
+
+def test_register_normal(authenticator):
+  assert 'name' in authenticator.users
+
+def test_register_already_exists_user(authenticator):
+  with pytest.raises(ValueError, match="エラー: ユーザーは既に存在します。"):
+    authenticator.register('name', 'password')
+
+def test_login_normal(authenticator):
+  assert authenticator.login('name', 'pass') == 'ログイン成功'
+
+def test_login_unmatch_password(authenticator):
+  with pytest.raises(ValueError, match="エラー: ユーザー名またはパスワードが正しくありません。"):
+    authenticator.login('name', '')
