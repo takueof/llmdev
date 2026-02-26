8c15eac6e63c817022c73da880257cf13b08a73f
Taku Watabe <taku.eof@gmail.com>
2026-02-26 16:56:10 +0900

    Implement static type in "06_test"

--- 06_test/authenticator.py
+++ 06_test/authenticator.py
@@ -1,16 +1,16 @@
 class Authenticator:
-    def __init__(self):
-        self.users = {}  # ユーザー情報を格納（インスタンス変数）
+    def __init__(self) -> None:
+        self.users = dict[str, str]()  # ユーザー情報を格納（インスタンス変数）
 
     # ユーザーの登録
-    def register(self, username, password):
+    def register(self, username: str, password: str) -> None:
         if username in self.users:
             raise ValueError("エラー: ユーザーは既に存在します。")
         else:
             self.users[username] = password
 
     # ログイン
-    def login(self, username, password):
+    def login(self, username: str , password: str) -> str | ValueError:
         if self.users.get(username) == password:
             return "ログイン成功"
         else:
--- 06_test/test_authenticator.py
+++ 06_test/test_authenticator.py
@@ -1,22 +1,23 @@
+from collections.abc import Iterator
 import pytest
 from authenticator import Authenticator
 
 @pytest.fixture
-def authenticator():
+def authenticator() -> Iterator[Authenticator]:
   auth = Authenticator()
   auth.register('name', 'pass')
   yield auth
 
-def test_register_normal(authenticator):
+def test_register_normal(authenticator: Authenticator) -> None:
   assert 'name' in authenticator.users
 
-def test_register_already_exists_user(authenticator):
+def test_register_already_exists_user(authenticator: Authenticator) -> None:
   with pytest.raises(ValueError, match="エラー: ユーザーは既に存在します。"):
     authenticator.register('name', 'password')
 
-def test_login_normal(authenticator):
+def test_login_normal(authenticator: Authenticator) -> None:
   assert authenticator.login('name', 'pass') == 'ログイン成功'
 
-def test_login_unmatch_password(authenticator):
+def test_login_unmatch_password(authenticator: Authenticator) -> None:
   with pytest.raises(ValueError, match="エラー: ユーザー名またはパスワードが正しくありません。"):
     authenticator.login('name', '')
